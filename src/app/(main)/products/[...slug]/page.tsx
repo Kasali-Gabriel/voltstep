4f623cc78a23d8a2dbf9e8ef41edebd64b33ff6c
Kasali Gabriel <kasaliGabriel6@gmail.com>
2025-07-26 12:28:36 +0100

    Changes to be committed: 	modified:   src/app/(main)/products/[...slug]/page.tsx

--- src/app/(main)/products/[...slug]/page.tsx
+++ src/app/(main)/products/[...slug]/page.tsx
@@ -10,10 +10,11 @@ export default async function Page({
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
   const { slug } = await params;
+  const searchParamsObj = await searchParams;
 
   const filters = parseFiltersFromURL(
     new URLSearchParams(
-      Object.entries(searchParams).map(([k, v]) => [
+      Object.entries(searchParamsObj).map(([k, v]) => [
         k,
         Array.isArray(v) ? v[0] : v || '',
       ]),
